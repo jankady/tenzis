927da77eaad93b65ed60e3d53b11a20a16d281f4
Jan Kaduch <kaduch05@gmail.com>
2025-11-29 19:58:27 +0100

    change ishold after click on dice

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ export default function App() {
 
     const [dice, setDice] = React.useState(generateAllNewDice)
 
+    // hold function
+    function hold(id) {
+        setDice(prevState => {
+            return prevState.map((diceObject) => {
+                if (diceObject.id === id) {
+                    return {...diceObject, isHeld: !diceObject.isHeld}
+                }
+                return diceObject
+            })
+        })
+    }
+
     function generateAllNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
@@ -21,7 +33,10 @@ export default function App() {
 
     // map over each dice and create Die component
     const diceElements = dice.map(object => {
-        return <Die key={object.id} value={object.value} isHeld={object.isHeld}/>
+        return <Die key={object.id}
+                    value={object.value}
+                    isHeld={object.isHeld}
+                    hold={()=> hold(object.id)}/>
     })
 
     return <main>
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,8 +1,7 @@
+export default function Die(props) {
 
-
-export default function Die(props){
-
-    return(<>
-        <button className="dice" style={{ background: props.isHeld ? "#59E391" : "#FFFFFF"}}>{props.value}</button>
+    return (<>
+        <button className="dice"
+                style={{background: props.isHeld ? "#59E391" : "#FFFFFF"}}  onClick={props.hold}>{props.value}</button>
     </>)
 }
